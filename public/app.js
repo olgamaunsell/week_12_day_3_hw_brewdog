5c2a877c58acc83bb305f78ca104abb0516b7c57
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2018-02-21 18:05:36 +0000

    mvp complete + dropdown extension

--- public/app.js
+++ public/app.js
@@ -1,5 +1,78 @@
 const app = function () {
-
+  const url = "https://s3-eu-west-1.amazonaws.com/brewdogapi/beers.json";
+  makeRequest(url, requestComplete);
 };
+
+const makeRequest = function (url, callback) {
+  const request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.addEventListener("load", callback);
+  request.send();
+}
+
+const requestComplete = function () {
+  if(this.status !== 200) return;
+  const jsonString = this.responseText;
+  const beers = JSON.parse(jsonString);
+  console.log("beers", beers[0]);
+  // populateList(beers);
+  populateDropdown(beers);
+}
+
+const populateList = function (beers) {
+  console.log("beers", beers);
+  const beerList = document.getElementById("beer-list");
+
+  beers.forEach(function (beer) {
+    const li = document.createElement("li");
+    li.innerText = beer.name;
+    const image = document.createElement("img");
+
+    image.height = "200";
+    image.src = beer.image_url;
+
+    beerList.appendChild(li);
+    beerList.appendChild(image);
+  });
+}
+
+const populateDropdown = function (beers) {
+  console.log(beers);
+  const dropdown = document.querySelector("select");
+
+  beers.forEach(function (beer, index) {
+    const option = document.createElement("option");
+    option.value = index;
+    option.innerText = beer.name;
+    dropdown.appendChild(option);
+  });
+
+  const handleSelectChanged = function () {
+
+    const selectedBeer = beers[this.value];
+
+    const beerUl = document.getElementById("beer-info");
+    beerUl.innerHTML = "";
+    const name = document.createElement('li');
+    name.innerText = `Beer name: ${selectedBeer.name}`
+
+    beerUl.appendChild(name);
+    const lineBreak = document.createElement('br');
+    beerUl.appendChild(lineBreak);
+    const image = document.createElement("img");
+
+    image.height = "200";
+    image.src = selectedBeer.image_url;
+
+    beerUl.appendChild(image);
+
+    // also storing last beer selected to local storage
+
+    const jsonString = JSON.stringify(selectedBeer);
+    localStorage.setItem('last beer selected', jsonString);
+
+  }
+  dropdown.addEventListener('change', handleSelectChanged);
+}
 
 document.addEventListener('DOMContentLoaded', app);
